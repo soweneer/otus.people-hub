bdf83f0ef831a4451ed817e44122ca0572e2be79
Ilia-Jan <i.lazarev.se@gmail.com>
2026-04-20 03:34:13 +0300

    quotes removed

--- src/PeopleHub.Infrastructure/Db/DbClient.cs
+++ src/PeopleHub.Infrastructure/Db/DbClient.cs
@@ -69,54 +69,53 @@ internal sealed class DbClient(string connectionString)
         const string query =
             #region SQL для создания базовых таблиц
             $"""
-                DROP TABLE IF EXISTS "{FriendsRequestsTable}";
-                DROP TABLE IF EXISTS "{AccountsTable}";
-                DROP TABLE IF EXISTS "{PersonsTable}";
+                DROP TABLE IF EXISTS {FriendsRequestsTable};
+                DROP TABLE IF EXISTS {AccountsTable};
+                DROP TABLE IF EXISTS {PersonsTable};
 
-                CREATE TABLE "{PersonsTable}" (
-                  "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                  "surname" VARCHAR(100) NOT NULL,
-                  "name" VARCHAR(100) NOT NULL,
-                  "age" SMALLINT NOT NULL,
-                  "gender" SMALLINT NOT NULL,
-                  "city" VARCHAR(100) NOT NULL,
-                  "bio" TEXT
+                CREATE TABLE {PersonsTable} (
+                    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                    surname VARCHAR(100) NOT NULL,
+                    name VARCHAR(100) NOT NULL,
+                    age SMALLINT NOT NULL,
+                    gender SMALLINT NOT NULL,
+                    city VARCHAR(100) NOT NULL,
+                    bio TEXT
                 );
 
-                CREATE TABLE "{FriendsRequestsTable}" (
-                  "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                  "sender_person_id" INTEGER NOT NULL,
-                  "receiver_person_id" INTEGER NOT NULL,
-                  "status" INTEGER NOT NULL DEFAULT 0,
-                  CONSTRAINT "friends_ibfk_1" FOREIGN KEY ("sender_person_id") REFERENCES "{PersonsTable}" ("id") ON DELETE CASCADE,
-                  CONSTRAINT "friends_ibfk_2" FOREIGN KEY ("receiver_person_id") REFERENCES "{PersonsTable}" ("id") ON DELETE CASCADE,
-                  CONSTRAINT "friends_relation_unique" UNIQUE ("sender_person_id", "receiver_person_id"),
-                  CONSTRAINT "friends_relation_unique_reverse" UNIQUE ("receiver_person_id", "sender_person_id")
+                CREATE TABLE {FriendsRequestsTable} (
+                    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                    sender_person_id INTEGER NOT NULL,
+                    receiver_person_id INTEGER NOT NULL,
+                    status INTEGER NOT NULL DEFAULT 0,
+                    CONSTRAINT friends_ibfk_1 FOREIGN KEY (sender_person_id) REFERENCES {PersonsTable} (id) ON DELETE CASCADE,
+                    CONSTRAINT friends_ibfk_2 FOREIGN KEY (receiver_person_id) REFERENCES {PersonsTable} (id) ON DELETE CASCADE,
+                    CONSTRAINT friends_relation_unique UNIQUE (sender_person_id, receiver_person_id),
+                    CONSTRAINT friends_relation_unique_reverse UNIQUE (receiver_person_id, sender_person_id)
                 );
 
-                CREATE TABLE "{AccountsTable}" (
-                  "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                  "email" VARCHAR(100) NOT NULL,
-                  "password" VARCHAR(100) NOT NULL,
-                  "person_id" INTEGER NOT NULL,
-                  CONSTRAINT "accounts_ibfk_1" FOREIGN KEY ("person_id") REFERENCES "{PersonsTable}" ("id") ON DELETE CASCADE
+                CREATE TABLE {AccountsTable} (
+                    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                    email VARCHAR(100) NOT NULL,
+                    password VARCHAR(100) NOT NULL,
+                    person_id INTEGER NOT NULL,
+                    CONSTRAINT accounts_ibfk_1 FOREIGN KEY (person_id) REFERENCES {PersonsTable} (id) ON DELETE CASCADE
                 );
-                
             """;
         
         // TODO uncomment me when got ready to do indexes homewwork
-        // CREATE INDEX "ix_{FriendsRequestsTable}_sender_person_id" ON "{FriendsRequestsTable}" ("sender_person_id");
-        // CREATE INDEX "ix_{FriendsRequestsTable}_receiver_person_id" ON "{FriendsRequestsTable}" ("receiver_person_id");
-        // CREATE INDEX "ix_accounts_person_id" ON "{AccountsTable}" ("person_id");
+        // CREATE INDEX ix_{FriendsRequestsTable}_sender_person_id ON {FriendsRequestsTable} (sender_person_id);
+        // CREATE INDEX ix_{FriendsRequestsTable}_receiver_person_id ON {FriendsRequestsTable} (receiver_person_id);
+        // CREATE INDEX ix_accounts_person_id ON {AccountsTable} (person_id);
         #endregion
 
-        await ExecuteCmdAsync(query, cmd =>
-        {
-            cmd.ExecuteNonQuery();
-        });
+        await ExecuteCmdAsync(query, cmd => cmd.ExecuteNonQuery());
     }
+    
+    public Task ExecuteNonQuery(string query, IEnumerable<(string, object)> parameters = null) =>
+        ExecuteCmdAsync(query, cmd => cmd.ExecuteNonQuery(), parameters);
 
-    public async Task<object> ExecuteScalarAsync(string query, IEnumerable<(string, object)> parameters)
+    public async Task<object> ExecuteScalarAsync(string query, IEnumerable<(string, object)> parameters = null)
     {
         var scalar = new object();
         await ExecuteCmdAsync(query, cmd =>
@@ -127,7 +126,7 @@ internal sealed class DbClient(string connectionString)
         return scalar;
     }
     
-    public async Task<DataTable> ExecuteDataTableAsync(string query, IEnumerable<(string, object)> parameters)
+    public async Task<DataTable> ExecuteDataTableAsync(string query, IEnumerable<(string, object)> parameters = null)
     {
         var dataTable = new DataTable();
         await ExecuteCmdAsync(query, cmd =>
